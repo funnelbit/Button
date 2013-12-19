611ed1b42de6d9f046198c5b5602d344d017d06c
funnelbit <funnelbit@hatena.ne.jp>
2013-12-20 02:28:46 +0900

    あああああああああああああああああああ

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,16 @@
 $(document).on('ready', function () {
-    $('#button').on('click', function () {
+
+    $('#button').on('mouseup', function () {
+
         var audio_uri = $($(this).find('audio')).attr('src');
         var audio = new Audio();
             audio.src = audio_uri;
             audio.play();
 
-        //$('#button-counter');
+        var counter = $('#counter');
+        var count = counter.text();
+        count++;
+
+        counter.text(count);
     });
 });
